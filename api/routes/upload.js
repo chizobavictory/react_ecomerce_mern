9ad4322df5b24ef0cb11bad78706c2ba62750a02
Chizoba Victory <43314673+chizobavictory@users.noreply.github.com>
2023-08-23 16:35:38 +0100

    upadate imports

--- api/routes/upload.js
+++ api/routes/upload.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { Router } from "express";
 import multer from "multer";
-import { verifyTokenAndAuthorization } from "../middlewares/auth.js";
+import { verifyToken } from "../middlewares/auth.js";
 import {avatar} from "../controller/uploadController.js"; // Check the path here
 const router = Router();
 
 // Use the router object to define your route, not 'app'
-router.post("/:id", verifyTokenAndAuthorization, avatar);
+router.post("/:id", verifyToken, avatar);
 
 export default router;
