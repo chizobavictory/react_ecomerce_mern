cf17995aa68c50d714a0d42653188cd3bdae2f1d
Chizoba Victory <43314673+chizobavictory@users.noreply.github.com>
2023-08-23 16:46:25 +0100

    upadate imports

--- api/routes/upload.js
+++ api/routes/upload.js
@@ -6,6 +6,6 @@ import {avatar} from "../controller/uploadController.js"; // Check the path here
 const router = Router();
 
 // Use the router object to define your route, not 'app'
-router.post("/:id", verifyToken, avatar);
+// router.post("/:id", verifyToken, avatar);
 
 export default router;
